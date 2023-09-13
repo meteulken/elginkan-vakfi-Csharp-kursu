ba30af80f46a629cde3dbdb24e812793327f8b45
Mete <mete.ulken71@gmail.com>
2023-09-13 19:31:44 +0300

    add gitignore

--- Ders1/Program.cs
+++ Ders1/Program.cs
@@ -1,4 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// Path: Ders1\Program.cs
 
