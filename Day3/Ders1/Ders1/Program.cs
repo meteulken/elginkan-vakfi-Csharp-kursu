88421443758f9f88ceeec67ad7a07857bf2220b3
Mete <mete.ulken71@gmail.com>
2023-09-20 19:07:11 +0300

    .

---
+++ Day3/Ders1/Ders1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+Console.Write("1 ile 4 arası sayi girniz :");
+
+byte a = Convert.ToByte((Console.ReadLine()));
+
+switch (a)
+{
+    case 1:
+        Console.WriteLine("1 girdiniz.");
+        break;
+    case 2:
+        Console.WriteLine("2 girdiniz.");
+        break;
+    case 3:
+        Console.WriteLine("3 girdiniz.");
+        break;
+    case 4:
+        Console.WriteLine("4 girdiniz.");
+        break;
+}
