5b77bb3e54fbb109dd7c8fcb3459c85847a0b456
Mete <mete.ulken71@gmail.com>
2023-09-21 20:08:48 +0300

    metotlar

---
+++ Day4/Ders1/Ders1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        ax();
+        Kullan.bx(4);
+      
+    }
+    static void ax()
+    {
+        Console.WriteLine("selm");
+    }
+   
+}
+class Kullan
+{
+
+    static Kullan()
+    {
+        Console.WriteLine("first");
+    }
+
+
+    public static void bx(int t)
+    {
+        Console.WriteLine(t);
+    }
+    string  cx()
+    {
+        return "ver";
+    }
+    string dx(int p)
+    {
+        return p.ToString();
+    }
+}
---
+++ Day4/Ders2/Ders2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        metot();
+    }
+    static void metot()
+    {
+        int x1, x2, y1, y2;
+
+        while (true)
+        {
+            Console.Write("x1: ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            if (x1 == 0)
+            {
+                Console.WriteLine("Sifirdan farkli");
+                continue;
+            }
+            break;
+        }
+        while (true)
+        {
+            Console.Write("y1: ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            if (y1 == 0)
+            {
+                Console.WriteLine("Sifirdan farkli");
+                continue;
+            }
+            break;
+        }
+        while (true)
+        {
+            Console.Write("x2: ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            if (x2 == 0)
+            {
+                Console.WriteLine("Sifirdan farkli");
+                continue;
+            }
+            break;
+        }
+        while (true)
+        {
+            Console.Write("y2: ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+            if (y2 == 0)
+            {
+                Console.WriteLine("Sifirdan farkli");
+                continue;
+            }
+            break;
+        }
+        double s = uzaklikhesapla(x1, y1, x2, y2);
+        if(s < 0)
+            Math.Abs(s);
+        Console.WriteLine("{0},{1} ve {2},{4} noktarlarının uzaklığı= {4}", x1, x2, y1, y2, s);
+
+    }
+    public static double uzaklikhesapla(int x1,int y1,int x2,int y2)
+    {
+        double mt = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+        return mt;
+        
+    }
+}
