8296087270d7a000237628f5be5309084d13f012
Mete <mete.ulken71@gmail.com>
2023-09-27 19:01:43 +0300

    .

---
+++ Day4/Ders3/Ders3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        kokdurumu(1,2,1);
+    }
+    static string kokdurumu(int a,int b, int c)
+    {
+        double delta = Math.Pow(b,2) - 4 * a * c;
+
+        kokler(a,b,c,delta);
+        if(delta > 0)
+        { return "kokler ciftdir";}
+        else if (delta == 0)
+        { return "kokler cift ve esittir"; }
+        else
+        { return "kokler sanaldir"; }
+    }
+    static void kokler(int a, int b, int c, double delta)
+    {
+        double birinci_kok = -b + Math.Sqrt(delta);
+        double ikinici_kok = -b + Math.Sqrt(delta);
+        Console.WriteLine($"{birinci_kok},{ ikinici_kok}");
+    }
+}
---
+++ Day4/Ders5/Ders5/Program.cs
@@ -0,0 +1,47 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass2 myc = new MyClass2();
+        myc.topla();
+       
+       
+    }
+}
+public class MyClass
+{
+    public MyClass()
+    {
+        Console.WriteLine("selam");
+    }
+
+    public void topla()
+    {
+        Console.WriteLine(3 + 5);
+    }
+    void cikar()
+    {
+        Console.WriteLine(3 - 5);
+    }
+    protected void kullan()
+    {
+        cikar();
+    }
+}
+    class MyClass2:MyClass
+    {
+       void al()
+        {
+        kullan();
+        topla();
+        }
+    }
+
+   
+    /*
+     * public : her yerden ulaşabilir.
+     * private : sadece tanımlandığı sınıfdan oluşur.
+     * protected : hem tanımlandığı sınıftan hemde miras alan sınıftan ulaşabilir.
+     * internal : aynı program içinden ulaşabilir.
+     */
