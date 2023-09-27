25d1fb3a3a2c64851909bf67198acbdd0af4cfec
Mete <mete.ulken71@gmail.com>
2023-09-27 19:04:45 +0300

    .

--- Day5/Ders1/Ders1/Program.cs
+++ Day5/Ders1/Ders1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Arac toyota = new Arac();
+        toyota.Aracisim = "Toyota";
+        toyota.AracModel = "Corolla";
+        toyota.renk = "Beyaz";
+        toyota.AracYas = 5;
+        toyota.km = 40000;
+        toyota.Segment = 'C';
+        toyota.SasiNo = "CV34324HJH41JSSA";
+
+        toyota.AracBilgileriGöster();
+        Console.WriteLine();
+
+
+        Arac taylan = new Arac();
+        toyota.Aracisim = "Taylan";
+        toyota.AracModel = "At";
+        toyota.renk = "Kahverengi";
+        toyota.AracYas = 1;
+        toyota.km = 4153634;
+        toyota.Segment = 'S';
+        toyota.SasiNo = "AT415KFDEM25326GL";
+
+        toyota.AracBilgileriGöster();
+        Console.Read();
+    }
+}
+class Arac
+{
+    public string Aracisim;
+    public string AracModel;
+    public string renk;
+    public byte AracYas;
+    public int km;
+    public char Segment;
+    public string SasiNo;
+    public void AracBilgileriGöster()
+    {
+        Console.Write("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n", Aracisim, AracModel, renk, AracYas, km, Segment, SasiNo);
+
+    }
+}
---
+++ Day5/Ders2/Ders2/Program.cs
@@ -0,0 +1,58 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Arac toyota = new Arac("Toyota", "Corolla", "Beyaz", 5, 40000, 'C', "CV34324HJH41JSSA");
+        Console.WriteLine();
+        Arac taylan = new Arac("Taylan", "At", "Kahverengi", 1, 4153634, 'S', "AT415KFDEM25326GL");
+        Console.Read();
+
+        DenizAraci da = new DenizAraci();
+
+        da.araccinsi = "Motoryat";
+        da.agirligi = 5000;
+        da.hizi = 20;
+
+        Console.Read();
+
+    }
+}
+class Arac
+{
+    public Arac(string Aracisim, string AracModel, string renk, byte AracYas, int km, char Segment, string SasiNo)
+    {
+        this.Aracisim = Aracisim;
+        this.AracModel = AracModel;
+        this.renk = renk;
+        this.AracYas = AracYas;
+        this.km = km;
+        this.Segment = Segment;
+        this.SasiNo = SasiNo;
+        AracBilgileriGöster();
+
+    }
+
+    public string Aracisim;
+    public string AracModel;
+    public string renk;
+    public byte AracYas;
+    public int km;
+    public char Segment;
+    public string SasiNo;
+
+    public void AracBilgileriGöster()
+    {
+        Console.Write("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n", Aracisim, AracModel, renk, AracYas, km, Segment, SasiNo);
+
+    }
+}
+
+class DenizAraci
+{
+    public string araccinsi;
+    public int agirligi;
+    public byte hizi;
+
+}
